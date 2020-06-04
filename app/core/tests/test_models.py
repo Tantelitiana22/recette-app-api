158c98f71d5ecd606113b7666d50c920721b0758
Tantelitiana22 <tantelitiana22@gmail.com>
2020-06-04 23:25:00 +0200

    Add correct command for management

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -8,7 +8,10 @@ class ModelTest(TestCase):
         """Test creating a new user  with email is successfully"""
         email = 'test@example.com'
         password = 'TestPassword'
-        user = get_user_model().objects.create_user(email=email, password=password)
+        user = get_user_model().objects.create_user(
+            email=email,
+            password=password
+        )
 
         self.assertEqual(user.email, email)
         self.assertTrue(user.check_password(password))
@@ -16,18 +19,27 @@ class ModelTest(TestCase):
     def test_new_user_email_normalized(self):
         """"Test the email for a new user is normalized"""
         email = "test@PARISDEV.COM"
-        user = get_user_model().objects.create_user(email=email, password='test123')
+        user = get_user_model().objects.create_user(
+            email=email,
+            password='test123'
+        )
 
         self.assertEqual(user.email, email.lower())
 
     def test_user_invalid_email(self):
         """Test creating user with no email raises error."""
         with self.assertRaises(ValueError):
-            get_user_model().objects.create_user(email=None, password='test123')
+            get_user_model().objects.create_user(
+                email=None,
+                password='test123'
+            )
 
     def test_create_new_superuser(self):
         """Test creating new superuser."""
-        user = get_user_model().objects.create_superuser(email='test@example.com', password='test123')
+        user = get_user_model().objects.create_superuser(
+            email='test@example.com',
+            password='test123'
+        )
 
         self.assertTrue(user.is_superuser)
         self.assertTrue(user.is_staff)
